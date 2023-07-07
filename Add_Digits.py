fd700ab023079fafa288401075b4c2273860facd
21A95A0123 <103180113+21A95A0123@users.noreply.github.com>
2023-07-07 11:12:10 +0530

    def add_digits(n):     s=0     while n>0:        r=n%10        s=s+r        n=n//10     return s
    a=int(input())
    while a>9:
        a=add_digits(a)
    print(a)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,13 +1,12 @@
 def add_digits(n):
     s=0
     while n>0:
-        r=n%10
-        s=s+r
-        n=n//10
+       r=n%10
+       s=s+r
+       n=n//10
     return s
-    
-    
-a= int(input())
+
+a=int(input())
 while a>9:
-    a= add_digits(a)
+    a=add_digits(a)
 print(a)
